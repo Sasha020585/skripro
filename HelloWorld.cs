f73da7ab852d8a213def9114342334934fed273f
Sasha020585 <agromykov@gmail.com>
2024-11-17 19:34:40 +0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame updatefvfbhh
+    void Start()
+    {
+        print("Hello,World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
