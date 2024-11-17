4c385b254a013daa5c48f67dbcda72237769115d
Sasha020585 <agromykov@gmail.com>
2024-11-17 19:57:08 +0400

    Add files via upload

---
+++ Destory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //уничтожует обьект в начале игры
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
